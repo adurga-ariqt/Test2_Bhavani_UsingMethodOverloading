32b35254a12fb7ba0b56ab6670c49a23bf844707
adurga-ariqt <adurga@ariqt.com>
2022-11-12 12:57:56 +0530

    Add project files.

---
+++ Test2_Bhavani_UsingMethodOverloading/AirthematicOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2_Bhavani_UsingMethodOverloading
+{
+    public  class AirthematicOperation
+    {
+        public void Add( int a,int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("The sum of two numbers is :" + sum);
+        }
+        public void Add(int a, int b ,int c)
+        {
+            int sum = a + b+c;
+            Console.WriteLine("The sum of Three numbers is :" + sum);
+        }
+
+    }
+}
---
+++ Test2_Bhavani_UsingMethodOverloading/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test2_Bhavani_UsingMethodOverloading;
+
+Console.WriteLine("Hello, World!");
+AirthematicOperation objAirthematicOperation = new AirthematicOperation();
+objAirthematicOperation.Add(7, 2);
+objAirthematicOperation.Add(7, 8, 9);
+
